75093f311a33564e009d11dac587a0ce68d13485
Md. Rasheduzzaman <mdrasheduzzamanakash@gmail.com>
2022-11-08 13:41:31 +0600

    add dashboard and show cart in dashboard

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
@@ -6,18 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  cartData: any = {
-    productName: 'Asus',
-    productShortCode: 'as-lap',
-    category: 'Fruits',
-    price: 4100,
-    description: 'good in condition',
-    imageUrl: 'https://m.media-amazon.com/images/I/51UHoxzInpL._AC_SY200_.jpg',
-    isBestAchived: 'no',
-    createdDate: '2022-11-07T18:00:00.000Z',
-    origin: 'India',
-    id: 1,
-  };
+  @Input() cartData: any;
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  cartList: any = [
+    {
+      productName: 'Asus',
+      productShortCode: 'as-lap',
+      category: 'Fruits',
+      price: 4100,
+      description: 'good in condition',
+      imageUrl:
+        'https://images-eu.ssl-images-amazon.com/images/G/31/img22/Fashion/Gateway/BAU/BTF-Refresh/May/PF_MF/MF-3-186-116._SY116_CB636110853_.jpg',
+      isBestAchived: 'no',
+      createdDate: '2022-11-07T18:00:00.000Z',
+      origin: 'India',
+      id: 1,
+    },
+    {
+      productName: 'Realme phone ',
+      productShortCode: 'ph',
+      category: 'Fruits',
+      price: 232,
+      description: 'very good ',
+      imageUrl:
+        'https://m.media-amazon.com/images/I/51UHoxzInpL._AC_SY200_.jpg',
+      isBestAchived: 'yes',
+      createdDate: '2022-11-07T18:00:00.000Z',
+      origin: 'India',
+      id: 2,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
